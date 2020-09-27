1528b2957bfc4ce539b3bb0d526a08036110a9e9
kbanaszc <58666746+kbanaszc@users.noreply.github.com>
2020-09-27 23:35:40 +0200

    teerw

--- script.js
+++ script.js
@@ -31,16 +31,46 @@ for (y in arr) {
 }
 
 let tablica = "";
+let holder = "";
 
 function but(x) {
-  if (tablica.length < 18) {
-    let dana = x.getAttribute("value");
+  let dana = x.getAttribute("value");
+  if (tablica.length < 13) {
     console.log(dana);
 
-    if (dana === "del") tablica=tablica.slice(0, -1);
-    else {
+    if (dana === "del") {
+      tablica = tablica.slice(0, -1);
+    } else if (dana === "-") {
+      if (holder === "") {
+        holder = tablica;
+        tablica = "";
+      } else {
+        holder = holder - tablica;
+        tablica = "";
+      }
+    } else if (dana === "=") {
+      tablica = holder
+    } else if (dana === "C") {
+      tablica = "";
+      holder = "";
+    } else if (dana === "%") {
+      let y = tablica * 0.01;
+      tablica = y.toString();
+    } else if (
+      "1" ||
+      "2" ||
+      "3" ||
+      "4" ||
+      "5" ||
+      "6" ||
+      "7" ||
+      "8" ||
+      "9" ||
+      ","
+    ) {
       tablica = tablica + dana;
     }
+
     wynik.innerHTML = tablica;
   }
 }
